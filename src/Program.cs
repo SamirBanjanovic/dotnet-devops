1f6460c9c3178a4b72ab41275ced06b5029b42b4
Samir Banjanovic <sbanj17@gmail.com>
2020-10-17 23:30:56 -0400

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SamirBanjanovic.DotNetCore.Tools.DevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SamirBanjanovic.DotNetCore.Tools.DevOps.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
